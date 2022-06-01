76249719fd69d8d116ebee2625ab0a0afe785eb8
n-bruno <tuf47023@temple.edu>
2022-05-31 21:09:17 -0400

    Challenge #0 completed.

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -5,23 +5,28 @@
 // Crawl() must be called concurrently)
 //
 // @hint: you can achieve this by adding 3 lines
-//
+
+// Similar to this example: https://go.dev/tour/concurrency/10
 
 package main
 
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
 // real crawler. It crawls until the maximum depth has reached.
-func Crawl(url string, depth int, wg *sync.WaitGroup) {
+func Crawl(url string, depth int, wg *sync.WaitGroup, throttle <-chan time.Time) {
 	defer wg.Done()
 
 	if depth <= 0 {
 		return
 	}
+
+	// Channel used to throttle threads
+	<-throttle
 
 	body, urls, err := fetcher.Fetch(url)
 	if err != nil {
@@ -35,7 +40,7 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 	for _, u := range urls {
 		// Do not remove the `go` keyword, as Crawl() must be
 		// called concurrently
-		go Crawl(u, depth-1, wg)
+		go Crawl(u, depth-1, wg, throttle)
 	}
 	return
 }
@@ -44,6 +49,7 @@ func main() {
 	var wg sync.WaitGroup
 
 	wg.Add(1)
-	Crawl("http://golang.org/", 4, &wg)
+	throttle := time.Tick(time.Second)
+	Crawl("http://golang.org/", 4, &wg, throttle)
 	wg.Wait()
 }
